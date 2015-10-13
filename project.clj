93402461fbe83bf07a95d4ba6fea2b8a0769f5f2
cjwebb <colinjameswebb@hotmail.com>
2015-10-13 21:48:53 +0100

    Latest

--- project.clj
+++ project.clj
@@ -6,9 +6,15 @@
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-json "0.4.0"]
-                 [ring-cors "0.1.7"]]
+                 [ring-cors "0.1.7"]
+                 [clj-time "0.11.0"]
+                 [com.taoensso/faraday "1.7.1"]
+                 [prismatic/schema "0.4.4"]
+                 [metosin/schema-tools "0.5.2"]
+                 [dire "0.5.3"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler auth.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
+
--- src/auth/handler.clj
+++ src/auth/handler.clj
@@ -3,7 +3,19 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.json :as middleware]
-            [ring.middleware.cors :refer [wrap-cors]]))
+            [ring.middleware.cors :refer [wrap-cors]]
+            [clj-time.core :as t]
+            [clj-time.format :as f]
+            [taoensso.faraday :as db]
+            [schema.core :as s]
+            [schema.coerce :as coerce]
+            [schema-tools.core :as st]
+            [dire.core :refer :all]))
+
+(def db-opts
+  {:access-key (System/getenv "MC_AWS_ACCESS_KEY")
+   :secret-key (System/getenv "MC_AWS_SECRET_KEY")
+   :endpoint "https://dynamodb.eu-west-1.amazonaws.com"})
 
 ; ---------- requirements ---------
 ; need to be able to sign up
@@ -35,14 +47,24 @@
             :date-created "2015-05-07 20:15:29.500"})
 ; need to lookup by token
 
+(defn get-token! [id]
+  (db/get-item db-opts :mc-auth-tokens {:token id}))
+
+; ------------ handlers --------
+(defn get-token-handler [id]
+  (let [data (get-token! id)]
+    (if (nil? data)
+      {:status 404}
+      {:body data})))
+
 ; ------------ routes ----------
 (defroutes app-routes
   (GET "/" [] {:body {:application-name "auth"}})
   (POST "/accounts/signup" [] "signup")
-  (POST "/accounts/login" [] "given user creds, return auth token")
+  (POST "/auth/login" [] {:body token})
   (POST "/accounts/logout" [] "given some user creds, remove them")
   (GET "/accounts" [] "?user-id= or ?email= return info of user, if creds provided")
-  (GET "/tokens/:id" [] "return token information")
+  (GET "/tokens/:id" [id] (get-token-handler id))
   (route/not-found "Not Found"))
 
 (def app
