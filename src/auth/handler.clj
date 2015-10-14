24fc54a323f31b07332b3b6e62eff3b2d90d6098
Colin Webb <colinjameswebb@hotmail.com>
2015-10-14 13:39:34 +0100

    Plan out Endpoints
    We have two concerns, accounts and auth-tokens. It seems as though it
    would be useful to return both things most of the time.
    
    The only time is would make sense not to return token information is if
    the request only specifies the account-id. At some point in the future,
    I suspect that account-ids will become viewable to other users... who
    could then steal the tokens if they knew the endpoint.
    
    You should only be able to see the token if you're brand-new, logging
    in, or already know it anyway.
    
    I also don't think we require lookup by account-id as an endpoint yet.
    It'll just be necessary when you have a token.

--- src/auth/handler.clj
+++ src/auth/handler.clj
@@ -17,54 +17,37 @@
    :secret-key (System/getenv "MC_AWS_SECRET_KEY")
    :endpoint "https://dynamodb.eu-west-1.amazonaws.com"})
 
-; ---------- requirements ---------
-; need to be able to sign up
-; need to be able to login
-; need to be able to logout
-; need to be able to authorise X-AUTH-HEADER is valid
-
-; also need an "auth filter" to add to other services
-
-; future work
-; - change password
-; - password reset
-; - refresh tokens
-; - delete tokens
-; - multi-user accounts (somehow)
-
 ; ---------- data -----------
-(def user {:id "6ca2e9c0-f4ed-11e4-b443-353a40402a60"
-           :name "Colin Webb"
-           :email "colin@mailinator"
-           :password "use https://github.com/weavejester/crypto-password"
-           :date-created "2015-05-07 20:15:29.500"
-           :date-modified "2015-05-07 20:15:29.500"})
-; need to lookup by id
-; need to lookup by email
+; dynamotable
+;   hash id (for normal lookup)
+;   secondary-index email (for login lookup)
+(def account {:id "6ca2e9c0-f4ed-11e4-b443-353a40402a60"
+              :name "Colin Webb"
+              :email "colin@mailinator"
+              :password "use https://github.com/weavejester/crypto-password"
+              :date-created "2015-05-07 20:15:29.500"
+              :date-modified "2015-05-07 20:15:29.500"})
 
-(def token {:token "use https://github.com/weavejester/crypto-random"
+; dynamotable
+;   hash id (for checking if token exists)
+;   secondary-index account-id (for login flow lookup)
+(def token {:id "use https://github.com/weavejester/crypto-random"
             :account-id "6ca2e9c0-f4ed-11e4-b443-353a40402a60"
             :date-created "2015-05-07 20:15:29.500"})
-; need to lookup by token
 
-(defn get-token! [id]
-  (db/get-item db-opts :mc-auth-tokens {:token id}))
-
-; ------------ handlers --------
-(defn get-token-handler [id]
-  (let [data (get-token! id)]
-    (if (nil? data)
-      {:status 404}
-      {:body data})))
+(defn account-and-token-response []
+  {:token (dissoc token :account-id)
+   :account (dissoc account :password)})
 
 ; ------------ routes ----------
 (defroutes app-routes
   (GET "/" [] {:body {:application-name "auth"}})
-  (POST "/accounts/signup" [] "signup")
-  (POST "/auth/login" [] {:body token})
-  (POST "/accounts/logout" [] "given some user creds, remove them")
-  (GET "/accounts" [] "?user-id= or ?email= return info of user, if creds provided")
-  (GET "/tokens/:id" [id] (get-token-handler id))
+  (POST "/auth/signup" [] {:body (account-and-token-response)}) ;given sign-up stuff, return all
+  (POST "/auth/login" [] {:body (account-and-token-response)}) ;given username+password, return all
+  (POST "/auth/logout" [] {:status 204}) ; delete token
+  ; don't need this yet
+  ;(GET "/auth/accounts/:id" [] {:body (dissoc account :password)}) ;given the account-id, don't show token info
+  (GET "/auth/tokens/:id" [] {:body (account-and-token-response)}) ;given token, show everything
   (route/not-found "Not Found"))
 
 (def app
